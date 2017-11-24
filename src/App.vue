33841e34fa62167fb70b3186776c72a87687dcd4
Oleksii Novikov <oleksii.novikov@gmail.com>
2017-11-24 16:11:49 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="container">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <app-header></app-header>
+      <div class="row">
+        <div class="col-xs-12">
+          <router-view/>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+  import Header from './components/Header';
 
+  export default {
+    components: {
+      appHeader: Header,
+    },
+  };
 </script>
 
 <style>
-.container {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    padding: 30px;
+  }
+  /*.container {*/
+    /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+    /*-webkit-font-smoothing: antialiased;*/
+    /*-moz-osx-font-smoothing: grayscale;*/
+    /*text-align: center;*/
+    /*color: #2c3e50;*/
+    /*margin-top: 60px;*/
+  /*}*/
 </style>

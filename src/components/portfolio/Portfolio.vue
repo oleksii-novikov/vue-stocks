020cda6077dde115089774fe7d670a9755b88bdd
Oleksii Novikov <oleksii.novikov@gmail.com>
2017-11-28 18:59:30 +0200

    Display stocks in profile page

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>The Portfolio - Portfolio component</h1>
+    <div>
+        <app-stock v-for="stock in stocks"
+                   v-bind:key="stock.id"
+                   v-bind:stock="stock"
+                   :stock="stock"></app-stock>
+    </div>
 </template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import Stock from './Stock';
+
+  export default {
+    computed: {
+      ...mapGetters({
+        stocks: 'stockPortfolio',
+      }),
+    },
+    components: {
+      appStock: Stock,
+    },
+  };
+</script>

1d6a639c78d75dd71af49153ff84b5f1c4d19532
Oleksii Novikov <oleksii.novikov@gmail.com>
2017-11-29 15:00:13 +0200

    Add validation

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -13,20 +13,26 @@
                             type="number"
                             class="form-control"
                             placeholder="Quantity"
-                            v-model.number="quantity">
+                            v-model.number="quantity"
+                            :class="{danger: insufficientFunds}">
                 </div>
                 <div class="pull-right">
                     <button
                         class="btn btn-success"
                         @click="buyStock"
-                        :disabled="quantity <= 0 || !Number.isInteger(quantity)"
-                        >Buy</button>
+                        :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
+                        >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 <script>
     export default {
       props: ['stock'],
@@ -34,6 +40,14 @@
         return {
           quantity: 0,
         };
+      },
+      computed: {
+        funds() {
+          return this.$store.getters.funds;
+        },
+        insufficientFunds() {
+          return this.quantity * this.stock.price > this.funds;
+        },
       },
       methods: {
         buyStock() {
